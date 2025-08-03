7a3d1a468b04d755a9e4df597e2fa35a31fec67e
Ojashri Chaudhari <ojashricaudhari26@gmail.com>
2025-08-03 23:50:18 +0530

    edit the env file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();   //to load .env
 
 export const connectDB =async()=>{
     try{
         // await mongoose.connect('mongodb://127.0.0.1:27017/shopzyUserdata');
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log('database connected')
     }catch(error){
         console.log('error in connecting', error)
